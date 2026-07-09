a26a77dc98a152e83fa5ef4b532260c491529b05
MahdiGoharjoo <mahdigoharjoo24@gmail.com>
2026-07-09 18:45:59 +0330

    18-04-1405-2mahdi

--- Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
+++ Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b712c53b6abebd94f6030721d8d4d6cc1477e8ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
